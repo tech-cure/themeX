fd27b10f355d79004b60051dc0ebbbe6a58eb480
Harsh <rafftar98@gmail.com>
2021-03-07 14:22:04 +0530

    Drop Down List

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+// To change Background Image
 function changeBackground(bg) {
     document.getElementById("main_page").style.backgroundImage = "url(img/" + bg + ".jpg)";
-}
+};
 
-function showText() {
-    
-}
+// To make DropDown List Work
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+    });
+  });
+  
